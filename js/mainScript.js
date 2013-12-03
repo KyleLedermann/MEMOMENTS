5929660c9fd5ea7e4637cbdcbf1ccd35fe47c044
Kyle Ledermann <kyle.ledermann@hotmail.com>
2013-12-02 21:34:04 -0500

    up

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,11 +1,17 @@
 
+		
+		 document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
     //Open or create the database
-    var db=openDatabase('contacts','1.0','my contacts app', 2 * 1024 * 1024);
+	 function onDeviceReady() {
     
+	}
+    var db=openDatabase('contacts','1.0','my contacts app', 2 * 1024 * 1024);
     //Initialize the database
     db.transaction(function(tx) {
       tx.executeSql('CREATE TABLE IF NOT EXISTS contacts(id integer primary key autoincrement, firstname, lastname, phonenumber)');
-    });
+    })
     
     function addContact() {
       var inputFirstName=document.getElementById("firstname").value;
